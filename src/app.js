5819027fa9737f230a6bd20fda5acd5ddd0aba4b
AlejandroGTA <36650624+AlejandroGTA@users.noreply.github.com>
2021-06-04 10:46:58 -0500

    Change Http

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+// const http = require('http');
+// const server = http.createServer(app);
 const path = require('path');
 const engine = require('ejs-mate');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cors = require('cors');
-const io = require('socket.io')(server,{
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true
-      }
-});
 
 app.set('views',path.join(__dirname,'Views'));
 app.engine('ejs',engine);
@@ -32,6 +24,6 @@ app.use(session({
 app.use('/', require('./Routes/Home'));
 
 const port = process.env.PORT || 5000;
-server.listen(port, '0.0.0.0', function(){
+app.listen(port, '0.0.0.0', function(){
     console.log("Server ON: " + port);
 });
