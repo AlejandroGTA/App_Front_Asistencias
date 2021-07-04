53268722c8341ebc3e899ab86d0cb59f7d0da404
AlejandroGTA <36650624+AlejandroGTA@users.noreply.github.com>
2021-07-03 21:28:28 -0500

    Change Google callback

--- src/Controller/authGoogle.js
+++ src/Controller/authGoogle.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const config = require('../appConfig');
 
 const GOOGLE_CLIENT_ID = "738248577660-8u1vvjog2htncs3ahm3mr2tgp9tjvuhm.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "WCrvucPZJnvWNQMQNTAvECBt";
@@ -7,16 +8,11 @@ const GOOGLE_CLIENT_SECRET = "WCrvucPZJnvWNQMQNTAvECBt";
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback",
+    callbackURL: config.APP_Google_Callback,
     passReqToCallback   : true
   },
   function(req, accessToken, refreshToken, profile, done) {
-    // console.log(profile);
-
     return done(null, profile);
-    // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    //   return done(err, user);
-    // });
   }
 ));
 
--- src/appConfig.js
+++ src/appConfig.js
@@ -1,4 +1,5 @@
 module.exports = {
     API_URL: "https://api-asistencias.herokuapp.com",
-    APP_URL: "https://app-front-asistencias.herokuapp.com/auth/google"
+    APP_URL: "https://app-front-asistencias.herokuapp.com/auth/google",
+    APP_Google_Callback: "https://app-front-asistencias.herokuapp.com/auth/google/callback"
 };
